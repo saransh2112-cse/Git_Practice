b64b3fc94cade3bfe23d6192bf4130a5213753d5
Saransh Gupta <74602862+saransh2112-cse@users.noreply.github.com>
2023-09-26 23:56:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 def addition(a,b,c):
     return a+b+c
 
-def subtraction(a,b,c):
-    return a-b-c
+def subtraction(a,b,e):
+    return a-b-e
 
 def division(a,b): ## DEVELOPER A STORY BRANCH
     return a/b
