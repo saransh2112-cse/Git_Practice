8b4c4aba585e558e19a05ee1d9ea96709e5c7c8a
Saransh Gupta <saranshgupta2112@gmail.com>
2023-09-26 23:52:42 +0530

    subtraction of c added

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 def addition(a,b):
     return a+b
 
-def subtraction(a,b):
-    return a-b
+def subtraction(a,b,c):
+    return a-b-c
 
 def division(a,b): ## DEVELOPER A STORY BRANCH
     return a/b
