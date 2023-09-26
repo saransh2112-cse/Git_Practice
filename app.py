c7a32f77e25962b0eb89090eabdedae3a0275cfc
Saransh Gupta <saranshgupta2112@gmail.com>
2023-09-26 23:58:12 +0530

    c replace by d

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 def addition(a,b,c):
     return a+b+c
 
-def subtraction(a,b,c):
-    return a-b-c
+def subtraction(a,b,d):
+    return a-b-d
 
 def division(a,b): ## DEVELOPER A STORY BRANCH
     return a/b
